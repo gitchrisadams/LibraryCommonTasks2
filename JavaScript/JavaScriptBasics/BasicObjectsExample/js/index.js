97b83c8bf80dedf968732b1e49215cedfda9862a
Christopher Adams <chrismichaeladams@gmail.com>
2017-04-20 07:39:59 -0400

    Javascript Arrays

--- JavaScript/JavaScriptBasics/BasicObjectsExample/js/index.js
+++ JavaScript/JavaScriptBasics/BasicObjectsExample/js/index.js
@@ -15,3 +15,37 @@ var e2 = new Employee("Chris");
 /** Output Employee names to browser. */
 document.write("E1 Name = " + e1.getName() + "<br>");
 document.write("E2 Name = " + e2.getName() + "<br>");
+
+// Set the name of the Employee 1 to John"
+e1.name = 'John'
+document.write("E1 Name Changed = " + e1.getName() + "<br>");
+
+// Can also use bracket notation for output:
+e2.securityLevel = 15;
+console.log("Employee e2 security level: " + e2['securityLevel'])
+
+// Assign a function to employee 2:
+e2.updateDueDate = function(){
+	return true;
+};
+
+// Returns true
+console.log(e2.updateDueDate());
+
+// Create an object using JSON notation {}
+var myJsonObject = {
+	// Properties:
+	name: 'Jason',
+	favoriteMovie: 'Friday the 13th',
+
+	// Function as object property.
+	putOnMask: function(){
+		return true;
+	}
+};
+
+// Console log the object:
+console.log("The ")
+
+
+
---
+++ Python/BoilerPlate.py
@@ -0,0 +1,19 @@
+""" Christopher Adams
+    4/18/17 
+	Code description here.
+"""
+# Import NetCDF4 library and Dataset module:
+import netCDF4
+from netCDF4 import Dataset
+import sys
+
+# Function def here:
+def findStandardTimeVars(url):
+	print("Hello world")
+
+# Main program
+def main():
+	# Code here
+
+if __name__ == '__main__':
+	sys.exit(main())
