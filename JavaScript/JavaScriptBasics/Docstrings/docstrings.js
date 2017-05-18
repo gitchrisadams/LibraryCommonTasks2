f081d6449c3832616ce660e531bbaed87fd94e4d
Christopher Adams <chrismichaeladams@gmail.com>
2017-05-18 06:52:44 -0400

    Added Dosctring and python info

---
+++ JavaScript/JavaScriptBasics/Docstrings/docstrings.js
@@ -0,0 +1,8 @@
+  /**
+   * Sends Google analytics data to GoogleTagManagerView.js
+   * @param {string} category - The category tag to send to gtm.
+   * @param {string} action - The action tag to send to gtm.
+   * @param {string} label - The label tag to send to gtm.
+   * @param {string} value - The value tag to send to gtm.
+   * @return {string} example - The string to return.
+   */
---
+++ Python/BoilerPlateExecutableFile.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# The file is made executable with:
+# chmod a+x BoilerPlateExecutableFile.py
+# and can now be run w/ ./BoilerPlateExecutableFile.py from terminal
+# Notice the user/bin/evn python at the top.
+
+# Christopher Adams
+# date here.
+# Code description here.
+
+# Imports
+import sys
+
+
+def your_function_here(url):
+    print("Hello world", url)
+
+
+def main():
+    your_function_here("http://www.google.com")
+
+if __name__ == '__main__':
+    sys.exit(main())
