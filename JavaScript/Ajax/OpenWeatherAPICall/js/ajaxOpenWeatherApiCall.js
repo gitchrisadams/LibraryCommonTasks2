478c3f2c62f704b4bd0925d155d166ae1c1056d6
Christopher Adams <chrismichaeladams@gmail.com>
2017-08-06 13:57:56 -0400

    Using Ajax with OpenWeatherMap.org Api call wrapped in a module

---
+++ JavaScript/Ajax/OpenWeatherAPICall/js/ajaxOpenWeatherApiCall.js
@@ -0,0 +1,34 @@
+"use strict";
+/**
+ * Returns the json data from openweathermap.org.
+ * This file is modularized using reveal pattern.
+ */
+var openWeatherApiCall = (function() {
+	var url = "http://api.openweathermap.org/data/2.5/weather?q=London,England";
+    var httpRequest;
+
+    /**
+     * Makes the request to openweathermap.org using api key.
+     * @param {string} The api key to use for openweathermap.org
+     */
+    function makeRequest(apiKey) {
+
+      httpRequest = new XMLHttpRequest();
+      httpRequest.onreadystatechange = responseMethod;   // When http request is ready, call responseMethod.
+      httpRequest.open('GET', url + '&appid=' + apiKey);
+      httpRequest.send();
+    }
+
+    // Handle XHR response.
+    function responseMethod() {
+      if (httpRequest.readyState === 4) {
+        console.log(httpRequest.responseText);
+        return httpRequest.responseText;
+      }
+    };
+
+    // Return only makeRequest, reponseMethod is a private method.
+    return {
+      makeRequest: makeRequest
+    }
+})();
---
+++ JavaScript/Ajax/OpenWeatherAPICall/js/apikeytemplate.js
@@ -0,0 +1,4 @@
+// Replace with api key here and rename file to apikey.js
+// or change import name in index.html.
+var apiKey = "YourAPIKeyHERE";
+openWeatherApiCall.makeRequest(apiKey);
