57e97524423328e5f979412a60e643fadcd92317
Christopher Adams <chrismichaeladams@gmail.com>
2017-08-20 16:48:34 -0400

    Add IOS swift playground files with Var, constants, datatypes

---
+++ IOS/Swift/Variables, Constants, and DataTypes.playground/Contents.swift
@@ -0,0 +1,42 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Normal variable declaration.
+var myAge : Int = 32
+
+myAge = 33
+
+// Constant declaration w/ let:
+let myName : String = "Chris"
+let myMiddle : String = "Michael"
+
+let myAgeInTenYears = myAge + 10
+
+// Concat strings:
+let myFullName = "\(myName) Adams"
+let myFullNameWithMiddle = "\(myName) \(myMiddle) Adams"
+
+// Let xcode infer the data type:
+let chrisInferred = "Chris Adams"
+let chrisAgeInferred = 41
+
+// Concat a name and a number value:
+let myDetails = "\(myFullName) And my age is: \(myAge)"
+
+let booleans : Bool = true
+
+let floatingPointNumber : Flot = 1.3
+let doubleValue : Double = 4.333333
+
+
+
+
+
+
+
+
+
+
+
+
