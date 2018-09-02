68e1a614c84d14077bc93110ee0182e9332e016e
Christopher Adams <chrismichaeladams@gmail.com>
2018-09-02 10:24:00 -0400

    Djano starter template

---
+++ Python/Frameworks/Django/djangoTemplateEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/christopheradams/Documents/webdev/LibraryCommonTasks2/Python/Frameworks/Django/djangoTemplateEnv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
