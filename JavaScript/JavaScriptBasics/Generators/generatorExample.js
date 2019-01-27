b82bdc87e15a5a9ce72246b01bf64987d4a8a736
Christopher Adams <chrismichaeladams@gmail.com>
2019-01-26 21:42:19 -0500

    Add generator inside generator example

--- JavaScript/JavaScriptBasics/Generators/generatorExample.js
+++ JavaScript/JavaScriptBasics/Generators/generatorExample.js
@@ -30,6 +30,7 @@ function* generator2() {
 
 const gen2 = generator2();
 
+console.log("");
 console.log(gen2.next());
 console.log(gen2.next());
 console.log(gen2.next());
@@ -38,3 +39,69 @@ console.log(gen2.next());
 
 // End of generator so will output undefined
 console.log(gen2.next());
+
+/*
+ * Another example of a Generator function w/ for of loop.
+ */
+function* generator3() {
+  yield "\nFoo";
+  yield "Fie";
+  yield "Foe";
+  yield "Fum";
+}
+
+const gen3 = generator3();
+var giantArray = [];
+
+for (let value of gen3) {
+  giantArray.push(value);
+}
+
+console.log('\ngiantArray:');
+console.log(giantArray);
+
+
+/*
+ * Puting a Generator inside a Generator example
+ */
+function* generatorInsideGenerator1() {
+  yield 1;
+  yield 2;
+  yield* generatorInsideGenerator2();
+  yield 6;
+  yield 7;
+}
+
+
+function* generatorInsideGenerator2() {
+  yield 3;
+  yield 4;
+  yield 5;
+}
+
+const genInsideGen = generatorInsideGenerator1();
+var genInsideGenVals = [];
+
+for (let value of genInsideGen) {
+  genInsideGenVals.push(value);
+}
+
+console.log("\ngenInsideGen value array:");
+console.log(genInsideGenVals);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
