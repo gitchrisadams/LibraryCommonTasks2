8a1656787a2f82a04e36abd204fe99aae133eba5
Christopher Adams <Christopher.Adams@turner.com>
2020-04-01 10:43:24 -0400

    Add sample Kotlin project

---
+++ AndroidCommonTaskLibrary/MyFirstKotlin/app/src/main/java/com/christopheradams/myfirstkotlin/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.christopheradams.myfirstkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun change (view: View) {
+        imageView.setImageResource(R.drawable.chestopen)
+    }
+}
